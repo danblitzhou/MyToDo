6a862d60cdbfe28eb50070f7a47d246f66447e48
Daniel Zhou <danzhou1810@gmail.com>
2016-06-08 00:33:27 -0400

    Created basic templates for projects

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+
+  <h1>Projects</h1>
+  <p>
+    This is my /resources/view/projects/index.blade.php.
+  </p>
+
+@endsection
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+
+  <h1>Show</h1>
+  <p>
+    This is my /resources/view/projects/show.blade.php.
+  </p>
+
+@endsection
