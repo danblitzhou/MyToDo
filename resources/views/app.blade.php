c633c339ad0ced765435b70ea460327755a8a7fa
Daniel Zhou <danzhou1810@gmail.com>
2016-06-08 00:32:56 -0400

    created master controller layout

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My Todo App</title>
+    <!-- import twitter boostrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">    
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </body>
+
+</html>
