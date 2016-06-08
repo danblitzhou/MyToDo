7c64e859ec1652ffd3a2bf13ed3082c27f1a3479
Daniel Zhou <danzhou1810@gmail.com>
2016-06-08 01:52:36 -0400

    Creatd basic projects show page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,9 +2,35 @@
 
 @section('content')
 
-  <h1>Show</h1>
-  <p>
-    This is my /resources/view/projects/show.blade.php.
-  </p>
+<div class="panel panel-default">
+   <div class="panel-heading">
+     {{ $project->name}} 's tasks
+   </div>
+@if ( !$project->tasks->count() )
+   <div class="alert alert-danger" role="alert">{{ $project->name }} has no tasks. </div>
+@else
+   <table class="table">
+     <thead>
+       <tr>
+         <th>#</th>
+         <th>Name</th>
+         <th>Created at</th>
+         <th>Last updated</th>
+       </tr>
+     </thead>
+     <tbody>
+       @foreach( $project->tasks as $task)
+       <tr>
+         <td>{{$task->id}}</td>
+         <td><a href="{{ route('projects.tasks.show', [$project->slug, $task->slug]) }}">{{ $task->name }}</a></td>
+         <td>{{$task->created_at}}</td>
+         <td>{{$task->updated_at}}</td>
+       </tr>
 
+       @endforeach
+     </tbody>
+
+ </table>
+</div>
+  @endif
 @endsection
